a5dfc9f68a740f003a2922d446181794490e82c4
Gabriella Cannon <gabriellakate04@gmail.com>
2020-05-20 22:39:19 +0200

    manipulating hamburger and modal with classList

--- hosting_app/shared.js
+++ hosting_app/shared.js
@@ -1,20 +1,37 @@
 let backdrop = document.querySelector(".backdrop");
 let modal = document.querySelector(".modal");
-let selectPlanButtons = document.querySelectorAll(".plan button")
-let noButton = document.querySelector(".modal button")
+let selectPlanButtons = document.querySelectorAll(".plan button");
+let noButton = document.querySelector(".modal button");
+let toggleButton = document.querySelector(".toggle-button");
+let mobileNav = document.querySelector(".mobile-nav");
 
 for (let i = 0; i <  selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener("click", function () {
-        modal.style.display = "block";
-        backdrop.style.display = "block"
+        // modal.style.display = "block";
+        // backdrop.style.display = "block"
+        modal.classList.add("open");
+        backdrop.classList.add("open");
     })
 }
 
-backdrop.addEventListener("click", closeModal)
+backdrop.addEventListener("click", function () {
+    // mobileNav.style.display = "none";
+    mobileNav.classList.remove("open");
+    closeModal()
+});
 
 noButton.addEventListener("click", closeModal)
 
+toggleButton.addEventListener("click", function () {
+    // mobileNav.style.display = "block";
+    // backdrop.style.display = "block";
+    mobileNav.classList.add("open");
+    backdrop.classList.add("open");
+})
+
 function closeModal () {
-    backdrop.style.display = "none";
-    modal.style.display = "none"
+    // backdrop.style.display = "none";
+    // modal.style.display = "none"
+    modal.classList.remove("open");
+    backdrop.classList.remove("open");
 }
